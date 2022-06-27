5123ed537147b7b5bb21290be7651dc3f2a9fed0
Bhavya Khurana <69009796+TheNewC0der-24@users.noreply.github.com>
2022-06-27 09:12:29 +0530

    Create Header component

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import styles from './Header.module.css';
+import resume from '../../assets/resume.svg';
+
+const Header = () => {
+    return (
+        <div className={styles.container}>
+            <div className={styles.left}>
+
+                <h1 className={styles.heading}>
+                    A <span>Resume</span> that stands out !
+                </h1>
+                <h2 className={styles.heading}>
+                    Make your own resume. <span>It's free</span>
+                </h2>
+            </div>
+
+            <div className={styles.right}>
+                <img src={resume} alt="Resume" />
+            </div>
+        </div>
+    )
+}
+
+export default Header;
