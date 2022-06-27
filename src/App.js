3f8caf7731a61d1b173d892f5d494234641b9bea
Bhavya Khurana <69009796+TheNewC0der-24@users.noreply.github.com>
2022-06-27 09:13:15 +0530

    import Header component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
+import Header from './Components/Header/Header';
 
 function App() {
   return (
-    <div>
-      Hello
-    </div>
+    <>
+      <Header />
+    </>
   );
 }
 
