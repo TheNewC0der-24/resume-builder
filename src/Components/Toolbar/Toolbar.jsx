0b15bed58fd0faf00360a1ed609cfbbee645beb4
Bhavya Khurana <69009796+TheNewC0der-24@users.noreply.github.com>
2022-06-29 08:06:58 +0530

    Add bootstrap classes and create section object

--- src/Components/Toolbar/Toolbar.jsx
+++ src/Components/Toolbar/Toolbar.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import styles from './Toolbar.module.css';
+import Editor from '../Editor/Editor';
 
 import { BiDownload } from 'react-icons/bi';
 
@@ -7,21 +8,34 @@ const Toolbar = () => {
 
     const colors = ["#00b0ff", "#00bfa6", "#f50057", "#536dfe", "#f9a826"]
 
+    const sections = {
+        basicInfo: "Basic Info",
+        workExp: "Work Experience",
+        project: "Projects",
+        education: "Education",
+        achievements: "Achievements",
+        summary: "Summary",
+        others: "Others"
+    }
+
     return (
         <div className='container'>
             <h1 className='text-center my-5'>Resume Builder</h1>
-            <div className={styles.toolbar}>
-                <div className={styles.colors}>
+            <div className="row">
+                <div className={`${styles.colors} col-md-6 d-flex justify-content-center gap-3`}>
                     {colors.map((item) => (
                         <span
                             key={item}
-                            style={{ backgroundColor: item }}
-                            className={styles.color}
+                            style={{ backgroundColor: item, width: "30px", height: "30px" }}
+                            className={`${styles.colorBtn} btn rounded-5`}
                         />
                     ))}
                 </div>
-                <button>Download<BiDownload className='fs-4' /></button>
+                <div className={`${styles.margin} col-md-6 text-center`}>
+                    <button className={`${styles.downloadBtn} btn btn-primary`}>Download<BiDownload className='fs-4 ms-1' /></button>
+                </div>
             </div>
+
         </div>
     )
 }
