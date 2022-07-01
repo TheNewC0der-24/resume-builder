431b5cfd2edb3c17389b1481ecd39b6e5171a307
Bhavya Khurana <69009796+TheNewC0der-24@users.noreply.github.com>
2022-07-01 21:16:12 +0530

    Improve UI design

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -5,14 +5,17 @@ import resume from '../../assets/Resume.svg';
 const Header = () => {
     return (
         <div className={styles.container}>
-            <div className={styles.left}>
-
+            <div>
                 <h1 className={styles.heading}>
-                    A <span className='fw-bold'>Resume</span> that stands out !
+                    A <span className='fw-bold'>Resume</span> that stands out!
                 </h1>
-                <h2 className={styles.heading}>
+                <h3 className={styles.heading}>
                     Make your own resume. <span className='fw-bold'>It's free</span>
-                </h2>
+                </h3>
+                <h1 className={`${styles.heading} mt-4`}>You're just minutes away from a <span>polished, professional</span> RESUME.</h1>
+                <p className={`${styles.heading} ${styles.para} p-2 mt-3`}>
+                    Take the next step toward the job you want with an impressive, eye-catching RESUME.
+                </p>
             </div>
 
             <div className={styles.right}>
