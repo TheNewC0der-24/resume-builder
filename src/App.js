90dc5ed0732f051c1a8b0858e1bf2295bae3a922
Bhavya Khurana <69009796+TheNewC0der-24@users.noreply.github.com>
2022-07-01 23:39:43 +0530

    Import Info and footer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import './App.css';
 import Header from './Components/Header/Header';
+import Info from './Components/Info/Info';
 import Toolbar from './Components/Toolbar/Toolbar';
+
+import Footer from './Components/Footer/Footer';
 
 function App() {
   return (
     <>
       <Header />
+      <Info />
       <Toolbar />
+      <Footer />
     </>
   );
 }
